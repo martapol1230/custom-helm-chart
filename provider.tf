e170c30779a418589d318ac3b6959a38036fef80
martapol1230 <111398868+martapol1230@users.noreply.github.com>
2023-02-23 01:51:04 -0600

    added provider

--- provider.tf
+++ provider.tf
@@ -6,3 +6,13 @@ provider "google-beta" {
   region = var.gke_config["region"]
   zone   = var.gke_config["zone"]
 }
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
