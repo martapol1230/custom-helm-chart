f1c397574661d2713f27a1f283d3e5a26c2e9452
martapol1230 <111398868+martapol1230@users.noreply.github.com>
2023-02-23 01:20:10 -0600

    added variables

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "app_version" {
+  type = string
+}
+
+
+variable "app_port" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+}
+
+variable "google_domain_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "project_id" {
+  type = string
+}
+
+
+variable "environment" {
+  type = string
+}
+
+
+variable "repository" {
+  type = string
+}
