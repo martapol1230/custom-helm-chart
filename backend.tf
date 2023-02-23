73658e4ae6c4aa44012ac21c55e116f510e3f522
martapol1230 <111398868+martapol1230@users.noreply.github.com>
2023-02-23 00:20:09 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "terraform-project-marta"
-    prefix = "/dev/mnt/martapol1230/project_infrastructure/1.cluster_setup"
+    prefix = "/dev/mnt/martapol1230/custom_helm_chart/cluster_setup"
   }
 }
